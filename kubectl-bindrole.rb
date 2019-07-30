a92637d65861fba39bc75b50b7a969bb1b136c2e
Aya Igarashi <ladiclexxx@gmail.com>
2019-07-30 14:58:18 +0900

    Release v1.1.0

--- kubectl-bindrole.rb
+++ kubectl-bindrole.rb
@@ -3,8 +3,8 @@ require "formula"
 class KubectlBindrole < Formula
   desc "The kubectl plugin to fetch bound Role/ClusterRole to the specified SA, Group and User!"
   homepage "https://github.com/Ladicle/kubectl-bindrole"
-  url "https://github.com/Ladicle/kubectl-bindrole/releases/download/v1.0.0/kubectl-bindrole_darwin-amd64.tar.gz"
-  sha256 "d1aaee54682e8e16d339ccf96cc52d825a022ff4b3cd50d12199ccba8b0dcfcc"
+  url "https://github.com/Ladicle/kubectl-bindrole/releases/download/v1.1.0/kubectl-bindrole_darwin-amd64.tar.gz"
+  sha256 "7e0da62a72041f482a99ed18dc908e57426e2cfc57a99200cf3e760be7501031"
 
   def install
     bin.install "kubectl-bindrole"
