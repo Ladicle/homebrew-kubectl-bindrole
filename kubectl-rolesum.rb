df70983dc7bc8242f09fafde1163d1d246bba432
Aya Igarashi <ladiclexxx@gmail.com>
2020-07-08 22:00:29 +0900

    Fix sha256

--- kubectl-rolesum.rb
+++ kubectl-rolesum.rb
@@ -4,7 +4,7 @@ class KubectlRolesum < Formula
   desc "Summarize k8s RBAC roles to specified SA, Group and User!"
   homepage "https://github.com/Ladicle/kubectl-rolesum"
   url "https://github.com/Ladicle/kubectl-rolesum/releases/download/v1.4.0/kubectl-rolesum_darwin-amd64.tar.gz"
-  sha256 "aaa1780cdef58cb7711a323af7d78bf8ab1b637ebab2bab9fd969dcfc8514a79"
+  sha256 "f5fc2ab889bb0ddf47bc9e467eb6517ea7c876f7262a016d0658e15fd11aa366"
   def install
     bin.install "kubectl-rolesum"
   end
