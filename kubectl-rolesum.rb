623662bcfcd7a73a6029f1ca7945e8020c01d842
Aya Igarashi <aigarash@zlab.co.jp>
2020-07-20 10:28:05 +0900

    Release v1.5.0

--- kubectl-rolesum.rb
+++ kubectl-rolesum.rb
@@ -3,8 +3,8 @@ require "formula"
 class KubectlRolesum < Formula
   desc "Summarize k8s RBAC roles to specified SA, Group and User!"
   homepage "https://github.com/Ladicle/kubectl-rolesum"
-  url "https://github.com/Ladicle/kubectl-rolesum/releases/download/v1.4.0/kubectl-rolesum_darwin-amd64.tar.gz"
-  sha256 "f5fc2ab889bb0ddf47bc9e467eb6517ea7c876f7262a016d0658e15fd11aa366"
+  url "https://github.com/Ladicle/kubectl-rolesum/releases/download/v1.5.0/kubectl-rolesum_darwin-amd64.tar.gz"
+  sha256 "97fd3538976ce72f047850839c4785b43bd188f36b6b30ec73b8838b5261cd9e"
   def install
     bin.install "kubectl-rolesum"
   end
